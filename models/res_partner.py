4fdf03a29770bfd1af038f5d6ab7555de32f086a
Gonzalo Zelaya <gonzalo.zelaya@outsourcearg.com>
2024-10-24 16:09:46 -0300

    Added 5 stars

--- models/res_partner.py
+++ models/res_partner.py
@@ -12,18 +12,24 @@ class ResPartner(models.Model):
         ('2', 'Mal'),
         ('3', 'Bueno'),
         ('4', 'Muy bueno'),
+        ('5', 'Excelente'),
+        ('6', 'God'),
     ], 'Valoración financiera')
     val_tecnica = fields.Selection([
         ('1', 'Pésimo'),
         ('2', 'Mal'),
         ('3', 'Bueno'),
         ('4', 'Muy bueno'),
+        ('5', 'Excelente'),
+        ('6', 'God'),
     ], 'Valoración técnica')
     cal_producto = fields.Selection([
         ('1', 'Pésimo'),
         ('2', 'Mal'),
         ('3', 'Bueno'),
         ('4', 'Muy bueno'),
+        ('5', 'Excelente'),
+        ('6', 'God'),
     ], 'Calidad de los productos')
     
     nuevo_campo = fields.Selection([
@@ -31,6 +37,8 @@ class ResPartner(models.Model):
         ('2', 'Mal'),
         ('3', 'Bueno'),
         ('4', 'Muy bueno'),
+        ('5', 'Excelente'),
+        ('6', 'God'),
     ], 'Calidad de los productos')
     
    
